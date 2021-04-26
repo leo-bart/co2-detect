cda398fb029b812d5a88d26ff3e750380532fe18
leo-bart <lbaruffas@gmail.com>
2021-04-26 08:36:19 -0300

    Added plot script
    Added python script that allows to read from serial port, plot the concentration, and save data to a variable (that can be written to an output file eventually).

---
+++ readDataFromSerial.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+This script reads data from the serial port, plots a graphic with values 
+measured on the fly and saves information to a variable called data_list
+"""
+
+import serial
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+data_list = [['Tempo','concentração_ppm','resistência_ohm']]
+
+# This function is called periodically from FuncAnimation
+def animate(i, xs, ys):
+
+    # Read from Serial
+    data = ser.readline()
+    y = data.decode()
+    y = [float(i) for i in str.split(y,'|')] # converts to float list
+    #print(y[0])
+    
+
+    # Add x and y to lists
+    time = i * 1/sampleFreqHz
+    xs.append(time)
+    ys.append(y[0])
+    
+    # saves data every minute
+    if (time%60 == 0):
+        print('flush')
+        data_list.append([dt.datetime.today(),y[0],y[1]])
+
+
+    # Limit x and y lists to 20 items
+    xs = xs[-100:]
+    ys = ys[-100:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
+
+    # Format plot
+    ax.set_ylim([300, 1000])
+    plt.xticks(rotation=90, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('MQ135 read')
+    plt.ylabel('Concentração de $CO_2$/ppm')
+    plt.xlabel('Tempo/s')
+    plt.grid()
+
+
+
+# main program
+plt.ion()
+fig=plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+sampleFreqHz = 2.0 
+
+i=0
+xs=list()
+ys=list()
+i=0
+# the serial port must be changed accordingly
+ser = serial.Serial('/dev/ttyUSB0',9600)
+ser.close()
+ser.open()
+
+
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys),
+                                  interval=1000*1./sampleFreqHz)
+plt.show()
+
+
+
+    
